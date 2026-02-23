fee25f6c513b85133d5192bbe2d73d32358c01d8
Sonu Kumar <sonuramashishnpm@gmail.com>
2026-02-23 19:09:55 +0530

    Update entrypoint.sh

--- NPMRAG-API-Framework./entrypoint.sh
+++ NPMRAG-API-Framework./entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-
-# Exit if anything fails
 set -e
 
-# Optional: any setup commands you need
-echo "Starting FastAPI server..."
+export OLLAMA_HOST=127.0.0.1:11434
 
-# Start the FastAPI server on the port specified by HF Spaces
-uvicorn app:app --host 0.0.0.0 --port $PORT
+ollama serve &
+echo "Waiting for Ollama..."
+
+while ! nc -z localhost 11434; do
+  sleep 1
+done
+
+ollama pull llama3.2
+ollama pull qwen2.5-coder:7b
+
+echo "Starting FastAPI..."
+exec python3 -m uvicorn app:app --host 0.0.0.0 --port ${PORT:-7860}
