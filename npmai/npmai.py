ccfdb3365487a6e7b77bb2baa8f73b2bb9e1f3e4
Sonu Kumar <sonuramashishnpm@gmail.com>
2026-02-26 20:40:18 +0530

    Update npmai.py

--- npmai/npmai.py
+++ npmai/npmai.py
@@ -11,20 +11,44 @@ class Ollama(LLM):
     temperature: float = 0.3
     validated_schema: Optional[Dict[str,Any]]=None
     api: str = "https://npmai-api.onrender.com/llm"
-
+    
     @property
     def _llm_type(self) -> str:
         return "npmai"
 
     def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        Model_links = {
+            "llama3.2": "https://sonuramashish22028704-npmai.hf.space/llm",
+            "qwen2.5-coder:7b":"https://sonuramashish22028704-npmai.hf.space/qwen",
+            "vicuna:7b":"https://sonuramashish22028704-vicuna7b.hf.space/llm",
+            "gemma2:9b":"https://sonuramashish22028704-vicuna7b.hf.space/gemma",
+            "internlm2:7b":"https://sonuramashish22028704-internlm27b.hf.space/llm",
+            "maxkb/baichuan2:13b-chat":"https://sonuramashish22028704-internlm27b.hf.space/baichuan",
+            "falcon:7b-instruct":"https://sonuramashish22028704-falcon7binstruct.hf.space/llm",
+            "codellama:7b-instruct":"https://sonuramashish22028704-falcon7binstruct.hf.space/codellama",
+            "mistral:7b":"https://sonuramashish22028704-mistral7b.hf.space/llm",
+            "phi3:medium":"https://sonuramashish22028704-phi3medium.hf.space/llm",
+        }
+        
         payload = {
             "prompt": prompt,
             "model": self.model,
             "temperature": self.temperature,
             "validated_schema":self.validated_schema,
         }
-        response = requests.post(self.api, json=payload)
-        response.raise_for_status()
+
+        fallback_payload ={
+            "prompt":prompt,
+            "temperature":self.temperature,
+        }
+        try:
+            response = requests.post(self.api, json=payload)
+            response.raise_for_status()
+        except:
+            api = Model_links[self.model]
+            response = requests.post(api, json=fallback_payload)
+            response.raise_for_status()
+            
         data=response.json()
         if "response" in data:
             return data["response"]
@@ -62,21 +86,24 @@ class Memory:
         return string_history
 
 class Rag:
-    def __init__(self, files,query=None,DB_PATH=None,temperature=None,model=None):
+    def __init__(self, files,query=None,DB_PATH=None,link=None,temperature=None,model=None):
         self.files=files
         self.query=query
         self.db_path=DB_PATH
+        self.link=link
         self.temperature=temperature
         self.model=model
     def send(self):
         files=self.files
         DB_PATH=self.db_path
+        link=self.link
         query=self.query
         model=self.model
         temperature=self.temperature
         data={
             "query":query,
             "DB_PATH":DB_PATH,
+            "link":link,
             "temperature":temperature,
             "model":model
         }
@@ -101,5 +128,3 @@ if __name__ == "__main__":
     response=llm.invoke(prompts)
 
     print(response)
-
-
